bcbd490c8e5fd5336835fdda2e03ec885af1627a
haleytolar <Haleytolarwrites@outlook.com>
2024-07-04 11:34:21 -0500

    created chat screen UI interface

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,6 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, KeyboardAvoidingView, Platform, TouchableOpacity } from 'react-native';
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 
 const Screen2 = ({ route, navigation }) => {
   const { name, background } = route.params;
@@ -10,17 +11,93 @@ const Screen2 = ({ route, navigation }) => {
 
   return (
     <View style={[styles.container, { backgroundColor: background }]}>
-      <Text>Hello Screen2!</Text>
+      <Chat />
     </View>
   );
-}
+};
+
+const Chat = () => {
+  const [messages, setMessages] = useState([]);
+
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages));
+  };
+
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000',
+          },
+          left: {
+            backgroundColor: '#FFF',
+          },
+        }}
+      />
+    );
+  };
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'This is a system message',
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, []);
+
+  return (
+    <View style={styles.chatContainer}>
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+      {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
+      <TouchableOpacity
+        accessible={true}
+        accessibilityLabel="More options"
+        accessibilityHint="Lets you choose to send an image or your geolocation."
+        accessibilityRole="button"
+        onPress={() => { /* handle press */ }}>
+        <View style={styles.button}>
+          {/* button content */}
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
-  }
+    alignItems: 'center',
+  },
+  chatContainer: {
+    flex: 1,
+    width: '100%',
+  },
+  button: {
+    // define button styles here
+  },
 });
 
 export default Screen2;
